9d288b12fe34d0c75c2b3652c5a56f0ee8042126
edenfobian-cmyk <edenfobian@gmail.com>
2026-06-24 17:23:40 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.graduates24.com"
+url = "https://www.graduates24.com/youth_opportunities"
 
 response = requests.get(url)
 
 with open("page.html", "w", encoding="utf-8") as f:
     f.write(response.text)
 
-print("Saved page")
+soup = BeautifulSoup(response.text, "html.parser")
+
+print("Title:")
+print(soup.title.text)
